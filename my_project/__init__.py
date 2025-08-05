32033630b828dc87122d9c3d225a710c285a8e1d
vungocMaiNguyen <160621574+vungocMaiNguyen@users.noreply.github.com>
2025-08-05 14:53:41 +0200

    Update __init__.py

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -3,4 +3,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('my_project')
 
-logger.info("my_project package loaded")
+class MyClass:
+    def my_method(self):
+        return "Hello World"
+
+logger.info("my_project loaded")
+
